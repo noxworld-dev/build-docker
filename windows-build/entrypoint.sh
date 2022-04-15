dabc107defab64c6c54d833b972064879292faa2
Evengard <evengard@trioptimum.ru>
2022-04-15 11:35:21 +0300

    Makeflags for multithreaded build

--- windows-build/entrypoint.sh
+++ windows-build/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 cd "${GITHUB_WORKSPACE}"
-
+git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 mkdir -p "${GITHUB_WORKSPACE}/build"
 cd "${GITHUB_WORKSPACE}/src"
 export GOPATH="${GITHUB_WORKSPACE}/gocache/path"
 export GOCACHE="${GITHUB_WORKSPACE}/gocache/cache"
-go run ./internal/noxbuild --os windows -o "${GITHUB_WORKSPACE}/build"
+go build -buildvcs=false -o noxbuild ./internal/noxbuild
+export LD_LIBRARY_PATH=/usr/i686-w64-mingw32/lib
+export C_INCLUDE_PATH=/usr/i686-w64-mingw32/include
+./noxbuild --os windows -o "${GITHUB_WORKSPACE}/build"
 cd "${GITHUB_WORKSPACE}/build"
 i686-w64-mingw32-ldd opennox.exe | sed 's/.*\=> \(.*\)/\1/' | grep -v -i "not found" | xargs -I{} cp {} .
 chmod -R a+r "${GITHUB_WORKSPACE}/gocache"

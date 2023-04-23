d3c5c14934e31f2c35c65abfb409e836b3219086
Evengard <evengard@trioptimum.ru>
2023-04-23 21:49:04 +0300

    Update to Go 1.20.3

--- linux-build/entrypoint.sh
+++ linux-build/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 cd "${GITHUB_WORKSPACE}"
-
+git config --global --add safe.directory "${GITHUB_WORKSPACE}"
 mkdir -p "${GITHUB_WORKSPACE}/build"
 cd "${GITHUB_WORKSPACE}/src"
 export GOPATH="${GITHUB_WORKSPACE}/gocache/path"
